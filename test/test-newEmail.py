d22407a6785bbebb73f5c3c52d3325c115ddfed7
Wakaru Himura <wakaru44@gmail.com>
2013-04-07 01:18:37 +0200

    added new tasks-newEmail test

---
+++ test/test-newEmail.py
@@ -0,0 +1,21 @@
+
+import unittest
+from nose.tools import * 
+
+import sys
+sys.path.append("/usr/share/google_appengine")
+
+from google.appengine.ext import testbed
+from google.appengine.ext import db
+import episode
+
+
+class test_new_email_handler(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def test_refactoring(self):
+        # TODO: refactorizar el metodo post
+        pass
+
+
